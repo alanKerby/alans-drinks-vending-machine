26df237483983d82e5d964aa8e3c1a9060984ba2
alanKerby <alankerby@gmail.com>
2022-01-06 15:06:50 +0000

    drink controller added

---
+++ src/main/java/com/alan/alansdrinksvendingmachine/controller/DrinkController.java
@@ -0,0 +1,50 @@
+package com.alan.alansdrinksvendingmachine.controller;
+
+import com.alan.alansdrinksvendingmachine.domain.Drink;
+import com.alan.alansdrinksvendingmachine.service.DrinkService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/alans-drinks-vending-machine")
+public class DrinkController {
+
+    private final DrinkService drinkService;
+
+    public DrinkController(DrinkService drinkService) {
+        this.drinkService = drinkService;
+    }
+
+    //create a drink
+    @PostMapping("/create-drink")
+    public ResponseEntity<Drink> createDrink(@RequestBody Drink drink) {
+        return new ResponseEntity<>(drinkService.create(drink), HttpStatus.CREATED);
+    }
+
+    //read all drinks
+    @GetMapping("read-all-drinks")
+    public ResponseEntity<List<Drink>> readAllDrinks() {
+        return new ResponseEntity<>(drinkService.readAll(), HttpStatus.OK);
+    }
+
+    //read drink by id
+    @GetMapping("read-drink-by-id/{id}")
+    public ResponseEntity<Drink> findDrinkByID(@PathVariable Long id) {
+        return new ResponseEntity<>(drinkService.readByID(id), HttpStatus.FOUND);
+    }
+
+    //update a drink
+    @PutMapping("update-drink-by-id/{id}")
+    public ResponseEntity<Drink> updateSingleDrink(@PathVariable Long id, @RequestBody Drink drink) {
+        return new ResponseEntity<>(drinkService.updateByID(id, drink), HttpStatus.ACCEPTED);
+    }
+
+    //delete drink by id
+    @DeleteMapping("delete-drink-by-id/{id}")
+    public ResponseEntity<Boolean> deleteSingleDrink(@PathVariable Long id) {
+        return new ResponseEntity<>(drinkService.deleteByID(id), HttpStatus.ACCEPTED);
+    }
+}
