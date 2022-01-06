298f30443e506184f17c1e64c8133f3748a51dcf
alanKerby <alankerby@gmail.com>
2022-01-06 14:55:03 +0000

    Initial commit. Blank spring boot project

---
+++ src/main/java/com/alan/alansdrinksvendingmachine/AlansDrinksVendingMachineApplication.java
@@ -0,0 +1,13 @@
+package com.alan.alansdrinksvendingmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlansDrinksVendingMachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlansDrinksVendingMachineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alan/alansdrinksvendingmachine/AlansDrinksVendingMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alan.alansdrinksvendingmachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlansDrinksVendingMachineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
