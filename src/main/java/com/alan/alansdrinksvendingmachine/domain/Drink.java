7c57c286e2af8f8ed42ca25551f73332d22067b2
alanKerby <alankerby@gmail.com>
2022-01-06 17:32:45 +0000

    testing for domain added (86% coverage)

--- src/main/java/com/alan/alansdrinksvendingmachine/domain/Drink.java
+++ src/main/java/com/alan/alansdrinksvendingmachine/domain/Drink.java
@@ -5,11 +5,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
+import java.util.Objects;
 
 @Entity
 public class Drink {
 
-    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotNull(message = "The drink must have a name")
     private String name;
@@ -21,40 +23,39 @@ public class Drink {
     private boolean hotMilk;
     private int sugarSyrupMl;
 
-//    public Drink(Long id, String name, int hotWaterML, int iceWaterMl, int coffeeBeansGrams, int milkML, boolean frothedMilk, boolean hotMilk, int sugarSyrupMl) {
-//        this.id = id;
-//        this.name = name;
-//        this.hotWaterML = hotWaterML;
-//        this.iceWaterMl = iceWaterMl;
-//        this.coffeeBeansGrams = coffeeBeansGrams;
-//        this.milkML = milkML;
-//        this.frothedMilk = frothedMilk;
-//        this.hotMilk = hotMilk;
-//        this.sugarSyrupMl = sugarSyrupMl;
-//    }
-//
-//    public Drink(String name, int hotWaterML, int iceWaterMl, int coffeeBeansGrams, int milkML, boolean frothedMilk, boolean hotMilk, int sugarSyrupMl) {
-//        this.name = name;
-//        this.hotWaterML = hotWaterML;
-//        this.iceWaterMl = iceWaterMl;
-//        this.coffeeBeansGrams = coffeeBeansGrams;
-//        this.milkML = milkML;
-//        this.frothedMilk = frothedMilk;
-//        this.hotMilk = hotMilk;
-//        this.sugarSyrupMl = sugarSyrupMl;
-//    }
-
-    public Drink() {
-
+    public Drink(Long id, String name, int hotWaterML, int iceWaterMl, int coffeeBeansGrams, int milkML, boolean frothedMilk, boolean hotMilk, int sugarSyrupMl) {
+        this.id = id;
+        this.name = name;
+        this.hotWaterML = hotWaterML;
+        this.iceWaterMl = iceWaterMl;
+        this.coffeeBeansGrams = coffeeBeansGrams;
+        this.milkML = milkML;
+        this.frothedMilk = frothedMilk;
+        this.hotMilk = hotMilk;
+        this.sugarSyrupMl = sugarSyrupMl;
     }
 
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
+    public Drink(String name, int hotWaterML, int iceWaterMl, int coffeeBeansGrams, int milkML, boolean frothedMilk, boolean hotMilk, int sugarSyrupMl) {
+        this.name = name;
+        this.hotWaterML = hotWaterML;
+        this.iceWaterMl = iceWaterMl;
+        this.coffeeBeansGrams = coffeeBeansGrams;
+        this.milkML = milkML;
+        this.frothedMilk = frothedMilk;
+        this.hotMilk = hotMilk;
+        this.sugarSyrupMl = sugarSyrupMl;
+    }
+
+    public Drink() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
@@ -118,5 +119,18 @@ public class Drink {
 
     public void setSugarSyrupMl(int sugarSyrupMl) {
         this.sugarSyrupMl = sugarSyrupMl;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Drink drink = (Drink) o;
+        return hotWaterML == drink.hotWaterML && iceWaterMl == drink.iceWaterMl && coffeeBeansGrams == drink.coffeeBeansGrams && milkML == drink.milkML && frothedMilk == drink.frothedMilk && hotMilk == drink.hotMilk && sugarSyrupMl == drink.sugarSyrupMl && name.equals(drink.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, hotWaterML, iceWaterMl, coffeeBeansGrams, milkML, frothedMilk, hotMilk, sugarSyrupMl);
     }
 }
